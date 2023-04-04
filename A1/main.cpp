a22560d53dddfccbd9c97dc362915f201d41b6e8
Nico Guth <nico.guth@tu-dortmund.de>
2023-04-04 15:43:36 +0200

    Converted from Eigen to using arrays

--- A1/main.cpp
+++ A1/main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
-#include <Eigen/Dense>
 #include <math.h>
 #include <fstream>
 
 using namespace std;
- 
-using Eigen::MatrixXd;
-using Eigen::ArrayXd;
-using Eigen::ArrayXXd;
  
 int main()
 {
@@ -15,13 +10,16 @@ int main()
     double x_max = 1.;
 
     double t_min = 0.;
-    double t_max = 5.;
+    double t_max = 4.;
 
-    int N = 3000; // number of timesteps
-    int M = 1000; // number of spacesteps
+    int N = 10000; // number of timesteps
+    int M = 2000; // number of spacesteps
     
     double h = (x_max - x_min) / (M - 0.5);
     double tau = (t_max - t_min) / (N - 1);
+
+    int frame_count = 500;
+    int frame_stride = N/frame_count;
 
     // check if stability condition (sigma = tau/h < 1) is true
     if (tau/h > 1) {
@@ -29,73 +27,86 @@ int main()
         exit(1);
     }
 
-    ArrayXd E(M);
-    ArrayXd B(M);
+    double E[M], B[M], E_old[M], B_old[M];
 
-    ArrayXXd E_arr(N,M);
-    ArrayXXd B_arr(N,M);
-
-    ArrayXd x_E = ArrayXd::LinSpaced(M, x_min,       x_min+(M-1.0)*h);
-    ArrayXd x_B = ArrayXd::LinSpaced(M, x_min+0.5*h, x_min+(M-0.5)*h);
+    // linspaces for the x values
+    double x_E[M], x_B[M]; 
+    for (int m=0; m<M; m++) {
+        x_E[m] = x_min + m*h;
+        x_B[m] = x_min + (m+0.5)*h;
+    }
 
     // initial conditions:
-    E = (abs(x_E) < 0.3).select(exp(-x_E*x_E/(2*0.1*0.1))*sin(20 * M_PI * x_E), 0 * x_E);
-    B = (abs(x_B) < 0.3).select(exp(-x_B*x_B/(2*0.1*0.1))*sin(20 * M_PI * x_B), 0 * x_B);
+    for (int m=0; m<M; m++) {
+        // Gaussian wave packet:
+        E[m] = abs(x_E[m])<0.3 ? exp(-x_E[m]*x_E[m]/(2*0.1*0.1))*sin(20 * M_PI * x_E[m]) : 0;
+        B[m] = abs(x_B[m])<0.3 ? exp(-x_B[m]*x_B[m]/(2*0.1*0.1))*sin(20 * M_PI * x_B[m]) : 0;
+        // Sine wave packet
+        //E[m] = abs(x_E[m])<0.3 ? sin(20 * M_PI * x_E[m]) : 0;
+        //B[m] = abs(x_B[m])<0.3 ? sin(20 * M_PI * x_B[m]) : 0;
+    }
 
-    // save initial conditions to the output matrix
-    E_arr.row(0) = E;
-    B_arr.row(0) = B;
+    // write header and initial conditions to the output file
+    ofstream file = ofstream("data/output.csv", ios::trunc);
+    file << "# n, t, E[0:(M-1)], B[0:(M-1)]\n";
+    file << 0 << ", " << t_min;
+    for (int m=0; m<M; m++) {
+        file << ", " << E[m];
+    }
+    for (int m=0; m<M; m++) {
+        file << ", " << B[m];
+    }
+    file << "\n";
+    file.close();
+    cout << "\rSimulating... " << 1 << "/" << N;
 
     for (int n=1; n<N; n++) {
+        // copy arrays
+        for (int m=0; m<M; m++) {
+            E_old[m] = E[m];
+            B_old[m] = B[m];
+        }
 
         // FD step E
-        double E_old = E[1];
-        E.tail(M-1) = E.tail(M-1) - (tau/h) * (B.tail(M-1) - B.head(M-1));
+        for (int m=1; m<M; m++) {
+            E[m] = E_old[m] - (tau/h) * (B_old[m] - B_old[m-1]);
+        }
         
         // Boundary conditions for E field (left)
-        //E[0] = E_old+(tau-h)/(tau + h)*(E[1]-E[0]);     // absorbing
+        //E[0] = E_old[1]+(tau-h)/(tau + h)*(E[1]-E[0]);   // absorbing
         //E[0] = 0;                                       // reflective
         //E[0] = E[M-1];                                  // periodic
-
-        // Antenna
-        double A = 0.5;
-        double omega = 10; 
-        E[0] = A*sin(omega*n*tau);
+        E[0] = 0.5 * sin(10 * n * tau);                 // antenna
 
         // FD step B
-        double B_old = B[M-2];
-        B.head(M-1) = B.head(M-1) - (tau/h) * (E.tail(M-1) - E.head(M-1));
+        for (int m=0; m<M-1; m++) {
+            B[m] = B_old[m] - (tau/h) * (E[m+1]-E[m]);
+        }
         
         // Boundary conditions for B field (right)
-        B[M-1] = B_old+(tau-h)/(tau+h)*(B[M-2]-B[M-1]); // absorbing
+        //B[M-1] = B_old[M-2]+(tau-h)/(tau+h)*(B[M-2]-B[M-1]); // absorbing
         //B[M-1] = 0;                                     // reflective        
-        //B[M-1] = B[0];                                  // periodic        
+        //B[M-1] = B[0];                                  // periodic
+        B[M-1] = 0.5 * sin(10 * n * tau);                 // antenna        
 
-        // save the current timestep
-        E_arr.row(n) = E;
-        B_arr.row(n) = B;
-        cout << "\rSimulating... " << n+1 << "/" << N;
+        // save the current timestep in regular intervals
+        if (((n+1) % frame_stride == 0) || (n==(N-1))) {   
+            file = ofstream("data/output.csv", ios::app);
+            file << n << ", " << t_min+n*tau;
+            for (int m=0; m<M; m++) {
+                file << ", " << E[m];
+            }
+            for (int m=0; m<M; m++) {
+                file << ", " << B[m];
+            }
+            file << "\n";
+            file.close();
+            cout << "\rSimulating... " << n+1 << "/" << N;
+        }
     }
     cout << "\n";
 
-    cout << "Writing files...\n";
-    ofstream file("data/output_E.csv", ios::trunc);
-    if (file.is_open())
-    {
-        file << "# E field (N rows, M columns)\n";
-        file << E_arr << '\n';
-    }
-    file.close();
-
-    file = ofstream("data/output_B.csv", ios::trunc);
-    if (file.is_open())
-    {
-        file << "# B field\n";
-        file << B_arr << '\n';
-    }
-    file.close();
-
-    // print metadata for plotting
+    // write metadata to a file for plotting 
     file = ofstream("data/output_constants.json", ios::trunc);
     if (file.is_open())
     {
--- A1/plot.py
+++ A1/plot.py
@@ -6,25 +6,21 @@ import json
 
 # %%
 print("Reading in data...")
-E_arr = np.genfromtxt("data/output_E.csv")
-B_arr = np.genfromtxt("data/output_B.csv")
 
 # read in the constants
 with open("data/output_constants.json", "r") as f:
     consts = json.load(f)
-
 M = consts["M"]
 x_min = consts["x_min"]
 x_max = consts["x_max"]
 h = consts["dx"]
 
-N = consts["N"]
-t_min = consts["t_min"]
-t_max = consts["t_max"]
-tau = consts["dt"]
-
-assert N == E_arr.shape[0] == B_arr.shape[0]
-assert M == E_arr.shape[1] == B_arr.shape[1]
+# read in the simulation data
+data = np.genfromtxt("data/output.csv", delimiter=",")
+n = data[:,0]
+t = data[:,1]
+E = data[:,2:2+M]
+B = data[:,2+M:2+2*M]
 
 x_E = np.linspace(x_min,       x_min + (M-1.0)*h, M)
 x_B = np.linspace(x_min+0.5*h, x_min + (M-0.5)*h, M)
@@ -32,26 +28,25 @@ x_B = np.linspace(x_min+0.5*h, x_min + (M-0.5)*h, M)
 # %%
 print("Plotting...")
 fig = plt.figure() 
-ax = plt.axes(xlim=(x_min, x_max), ylim=(-2, 2))
+ax = plt.axes(xlim=(x_min, x_max), ylim=(-1.5, 1.5))
 ax.set_xlabel("x")
 line_E, = ax.plot([], [], lw=2, label="E")
 line_B, = ax.plot([], [], lw=2, label="B")
 ax.legend(loc="upper right")
 
-frame_count = 200
-frame_stride  = N//frame_count
-assert(frame_count * frame_stride <= N)
+frame_count = E.shape[0]
 
 def init():  
     line_E.set_data([], [])
     line_B.set_data([], []) 
     return line_E, line_B 
 
-def animate(n):
-    if (n % 10 == 0): print(f"\rRendering... ({100*((n+10)/frame_count):.1f}%)", end="")
-    ax.set_title(f"t = {tau*n*frame_stride:.2f}")
-    line_E.set_data(x_E, E_arr[n*frame_stride,:])
-    line_B.set_data(x_E, B_arr[n*frame_stride,:])
+def animate(frame_idx):
+    if (frame_idx % 10 == 0) or (frame_idx == frame_count-1): 
+        print(f"\rRendering... ({100*((frame_idx+1)/frame_count):.1f}%)", end="")
+    ax.set_title(f"t = {t[frame_idx]:.2f}")
+    line_E.set_data(x_E, E[frame_idx,:])
+    line_B.set_data(x_E, B[frame_idx,:])
     return line_E, line_B
 
 anim = animation.FuncAnimation(
